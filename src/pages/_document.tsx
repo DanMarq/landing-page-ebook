bd2128eb0e55a50f9101788df366de6af97579ee
danmarq <danilo_marques@outlook.com>
2023-07-21 13:04:37 -0300

    adcionando noscript para teste

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Html, Head, Main, NextScript } from 'next/document'
 
 
@@ -6,6 +7,9 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
       <meta name="facebook-domain-verification" content="jv1nxz88g0dxw4c54mk2yhzigralfq" />
+      <noscript>
+            <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=851288690163546&amp;ev=PageView&amp;noscript=1" />
+          </noscript>
       </Head>
       <body>
         <Main />
