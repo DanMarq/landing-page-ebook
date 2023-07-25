2dbd4f81e93f47e46a217cb2dd340db51e868037
danmarq <danilo_marques@outlook.com>
2023-07-25 12:34:01 -0300

    corrigindo link da lp

--- src/pages/components/Button.tsx
+++ src/pages/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonContent {
 
 export default function Botao ({text, classStyle}:ButtonContent) {
     return (
-       <Link href={'https://bit.ly/3Q84mGa'}>
+       <Link href={'https://bit.ly/3Dt85q7'}>
          <Button className={classStyle}>{text}</Button>
        </Link>
     )
